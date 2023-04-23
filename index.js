12968709a0af8d393d03467210b94abcafba2a6d
JulianGonzalezLara <91323460+JulianGonzalezLara@users.noreply.github.com>
2023-04-23 14:59:40 +0200

    Deploy

--- index.js
+++ index.js
@@ -3,12 +3,12 @@ import dotenv from 'dotenv'
 import process from 'node:process'
 import IGNORED_USERS from './ignoredUsers.json' assert {type: 'json'}
 import {queryGPT} from './modules/gpt.js'
+import {queryGPTFirstMessage} from './modules/gpt.js'
 
 dotenv.config()
 const USER = process.env.USER
 const PASSWORD = process.env.PASSWORD
 const CHANNEL = 'Juli45G'
-// const IGNORED_USERS = ['streamelements']
 
 const client = new tmi.Client({
   options: { debug: false },
@@ -36,19 +36,20 @@ client.on('message', async (channel, tags, message, self) => {
       return
     }
 
-    const isChoosen = Math.floor(Math.random() * 5) === 0
+    const isChoosen = Math.floor(Math.random() * 3) === 0
 
     const isLongMessage = message.length > 30
 
     if(isChoosen && isLongMessage){
       const { total_tokens, content, type } = await queryGPT(message)
+      client.say(CHANNEL, `@${username} Personalidad: ${type}, ${content}`)
+      // console.log(`${displayName}: ${message}`)
+      // console.log(`${content} (${total_tokens}), (${type})`)
+    } else if(isFirstMessage){
+      const { total_tokens, content, type } = await queryGPTFirstMessage(message)
       client.say(CHANNEL, `@${username}, ${content}`)
-      console.log(`${displayName}: ${message}`)
-      console.log(`${content} (${total_tokens}), (${type})`)
     } else if(username === "juli45g"){
       const { total_tokens, content, type } = await queryGPT(message)
-      client.say(CHANNEL, `@${username}, ${content}`)
-      console.log(`${displayName}: ${message}`)
-      console.log(`${content} (${total_tokens}), (${type})`)
+      client.say(CHANNEL, `@${username} Personalidad: ${type}, ${content}`)
     }
 })
--- modules/gpt.js
+++ modules/gpt.js
@@ -4,7 +4,6 @@ import { getPersonality } from './personality.js'
 import fetch from 'node-fetch'
 
 dotenv.config({path: '../.env'})
-// dotenv.config()
 
 // const trim = (message) => message.trim().replace(/^"|"$/g, "")
 
@@ -35,7 +34,7 @@ export const queryGPT = async (message) => {
         stop: null
     }
 
-    // try {
+    try {
         const response = await fetch(API_URL, {
             method: 'POST',
             headers,
@@ -52,12 +51,65 @@ export const queryGPT = async (message) => {
             content,
             type
         }
-    // }catch (error) {
-    //     return {
-    //         total_tokens: 0,
-    //         content: 'No se ha podido completar la petición.',
-    //         type: 'error'
-    //     }
-    // }
+    }catch (error) {
+        return {
+            total_tokens: 0,
+            content: 'No se ha podido completar la petición.',
+            type: 'error'
+        }
+    }
+
+}
+
+export const queryGPTFirstMessage = async (message) => {
+    const KEY_GPT = process.env.KEY_GPT
+    const MAXCHARACTERS = 100
+    const MODEL = 'gpt-3.5-turbo'
+    const API_URL = 'https://api.openai.com/v1/chat/completions'
+
+    const cleanMessage = message.replaceAll(`"`, "'")
+
+    const prompt = `Imagina que eres un usuario del chat. Otro usuario ha escrito su primer mensaje, dale la bienbenida al canal con un mensaje que le anime a seguir viendo el directo. El contexto del chat es temática de videojuegos. Máximo ${MAXCHARACTERS} caracteres.`
+
+    const headers = {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${KEY_GPT}`
+    }
+
+    const body = {
+        messages:[{
+            "role": "user",
+            "content": prompt
+        }],
+        model: MODEL,
+        max_tokens: MAXCHARACTERS,
+        n: 1,
+        stop: null
+    }
+
+    try {
+        const response = await fetch(API_URL, {
+            method: 'POST',
+            headers,
+            body: JSON.stringify(body)
+        })
+
+        const data = await response.json()
+
+        const { total_tokens } = data.usage; 
+        const { content } = data.choices[0].message;
+
+        return {
+            total_tokens,
+            content,
+            type
+        }
+    }catch (error) {
+        return {
+            total_tokens: 0,
+            content: 'No se ha podido completar la petición.',
+            type: 'error'
+        }
+    }
 
 }
