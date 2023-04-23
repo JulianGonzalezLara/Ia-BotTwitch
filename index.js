1872b17262a47f6116f1a8e3fc9a05f402cf641e
JulianGonzalezLara <91323460+JulianGonzalezLara@users.noreply.github.com>
2023-04-23 15:29:46 +0200

    prueba

--- index.js
+++ index.js
@@ -60,5 +60,11 @@ client.on('message', async (channel, tags, message, self) => {
         return
       }
       client.say(CHANNEL, `@${username} Personalidad: ${type}, ${content}`)
+    }else if(username === "cheegoo_"){
+      const { total_tokens, content, type } = await queryGPT(message)
+      if (type === "error" || total_tokens === 0) {
+        return
+      }
+      client.say(CHANNEL, `/timeout cheegoo_ 1 por que si`)
     }
 })
