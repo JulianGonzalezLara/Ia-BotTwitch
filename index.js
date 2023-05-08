2a6b5d7e1dfe2f7ac95eb776af6bc52a57a53786
JulianGonzalezLara <91323460+JulianGonzalezLara@users.noreply.github.com>
2023-05-08 11:26:24 +0200

    prueba

--- index.js
+++ index.js
@@ -40,25 +40,25 @@ client.on('message', async (channel, tags, message, self) => {
 
     const isLongMessage = message.length > 30
 
-    if(isChoosen && isLongMessage){
-      const { total_tokens, content, type } = await queryGPT(message)
-      if (type === "error" || total_tokens === 0) {
-        return
-      }
-      client.say(CHANNEL, `@${username} Personalidad: ${type}, ${content}`)
-      // console.log(`${displayName}: ${message}`)
-      // console.log(`${content} (${total_tokens}), (${type})`)
-    } else if(isFirstMessage){
-      const { total_tokens, content } = await queryGPTFirstMessage(message)
-      if (total_tokens === 0) {
-        return
-      }
-      client.say(CHANNEL, `@${username}, ${content}`)
-    } else if(username === "juli45g"){
-      const { total_tokens, content, type } = await queryGPT(message)
-      if (type === "error" || total_tokens === 0) {
-        return
-      }
-      client.say(CHANNEL, `@${username} Personalidad: ${type}, ${content}`)
-    }
+    // if(isChoosen && isLongMessage){
+    //   const { total_tokens, content, type } = await queryGPT(message)
+    //   if (type === "error" || total_tokens === 0) {
+    //     return
+    //   }
+    //   client.say(CHANNEL, `@${username} Personalidad: ${type}, ${content}`)
+    //   // console.log(`${displayName}: ${message}`)
+    //   // console.log(`${content} (${total_tokens}), (${type})`)
+    // } else if(isFirstMessage){
+    //   const { total_tokens, content } = await queryGPTFirstMessage(message)
+    //   if (total_tokens === 0) {
+    //     return
+    //   }
+    //   client.say(CHANNEL, `@${username}, ${content}`)
+    // } else if(username === "juli45g"){
+    //   const { total_tokens, content, type } = await queryGPT(message)
+    //   if (type === "error" || total_tokens === 0) {
+    //     return
+    //   }
+    //   client.say(CHANNEL, `@${username} Personalidad: ${type}, ${content}`)
+    // }
 })
