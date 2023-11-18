ad3e45935cbf463db6f2c1bc72443000c2aa4b2b
JulianGonzalezLara <juliangl803@gmail.com>
2023-11-18 14:41:06 +0100

    Dependencies

--- index.js
+++ index.js
@@ -4,21 +4,22 @@ import process from 'node:process'
 import IGNORED_USERS from './ignoredUsers.json' assert {type: 'json'}
 import {queryGPT, queryGPTChiste} from './modules/gpt.js'
 import {queryGPTFirstMessage} from './modules/gpt.js'
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
 
 dotenv.config()
 const USER = process.env.USER
 const PASSWORD = process.env.PASSWORD
 const CHANNEL = 'Juli45G'
 
-const express = require("express");
-const app = express();
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
 
-// This line is important to ensure your app listens to the PORT env var
-const port = process.env.PORT ?? 8080;
-
-app.listen(port, () => {
-  console.log(`App listening on port ${port}`);
-});
+  // This line is important to ensure your app listens to the PORT env var
+  await app.listen(process.env.PORT, "0.0.0.0");
+  console.log(`Application is running on: ${await app.getUrl()}`);
+}
+bootstrap();
 
 const client = new tmi.Client({
   options: { debug: false },
